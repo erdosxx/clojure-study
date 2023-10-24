f41075c3e47fbb7e104416b679fa240a36ae30bf
Norel <norel.evoagile@gmail.com>
2023-10-24 16:54:47 +0900

    Add ex2 solution for FPO/ch1.

--- spec/clojure_study/functional_programming_for_the_object_oriented_programmer/chapter_1/functions_spec.clj
+++ spec/clojure_study/functional_programming_for_the_object_oriented_programmer/chapter_1/functions_spec.clj
@@ -38,6 +38,74 @@
                            (vec (range 1 11))))))
 
 
+(describe "my-third"
+          (it "for empty list return nil"
+              (should-be-nil (my-third '())))
+
+          (it "for empty vector return nil"
+              (should-be-nil (my-third [])))
+
+          (it "for one element list return nil"
+              (should-be-nil (my-third '(1))))
+
+          (it "for one element vector return nil"
+              (should-be-nil (my-third [1])))
+
+          (it "for two elements list return 2nd"
+              (should-be-nil (my-third '(1 2))))
+
+          (it "for two elements vector return 2nd"
+              (should-be-nil (my-third [1 2])))
+
+          (it "for three elements list return 2nd"
+              (should= 3 (my-third '(1 2 3))))
+
+          (it "for three elements vector return 2nd"
+              (should= 3 (my-third [1 2 3])))
+
+          (it "for 10 elements list return 2nd"
+              (should= 3 (my-third
+                           (eval (cons range '(1 11))))))
+
+          (it "for 10 elements list return 2nd"
+              (should= 3 (my-third
+                           (vec (range 1 11))))))
+
+
+(describe "my-third-2"
+          (it "for empty list return nil"
+              (should-be-nil (my-third-2 '())))
+
+          (it "for empty vector return nil"
+              (should-be-nil (my-third-2 [])))
+
+          (it "for one element list return nil"
+              (should-be-nil (my-third-2 '(1))))
+
+          (it "for one element vector return nil"
+              (should-be-nil (my-third-2 [1])))
+
+          (it "for two elements list return 2nd"
+              (should-be-nil (my-third-2 '(1 2))))
+
+          (it "for two elements vector return 2nd"
+              (should-be-nil (my-third-2 [1 2])))
+
+          (it "for three elements list return 2nd"
+              (should= 3 (my-third-2 '(1 2 3))))
+
+          (it "for three elements vector return 2nd"
+              (should= 3 (my-third-2 [1 2 3])))
+
+          (it "for 10 elements list return 2nd"
+              (should= 3 (my-third-2
+                           (eval (cons range '(1 11))))))
+
+          (it "for 10 elements list return 2nd"
+              (should= 3 (my-third-2
+                           (vec (range 1 11))))))
+
+
 (describe "my-apply"
 
           (it "applies + to a sequence of arguments"
--- src/clojure_study/functional_programming_for_the_object_oriented_programmer/chapter_1/functions.clj
+++ src/clojure_study/functional_programming_for_the_object_oriented_programmer/chapter_1/functions.clj
@@ -1,10 +1,25 @@
 (ns clojure-study.functional-programming-for-the-object-oriented-programmer.chapter-1.functions)
 
 
-(def my-second
-  (fn [list]
-    (if (< (count list) 2) nil
-        (nth list 1))))
+(defn my-second
+  "Ex1: Get second element ffrom list or vector"
+  [list]
+  (if (< (count list) 2) nil
+      (nth list 1)))
+
+
+(defn my-third
+  "Ex2: Get third element from list or vector"
+  [list]
+  (if (< (count list) 3) nil
+      (nth list 2)))
+
+
+(defn my-third-2
+  "Ex2: Get third element from list or vector"
+  [list]
+  (if (< (count list) 3) nil
+      (first (rest (rest list)))))
 
 
 (def my-apply
